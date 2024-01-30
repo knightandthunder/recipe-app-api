1f84c3c1fa243f80315ecda8ae5f61acffe3d396
knighandthunder <poudelanil60@gmail.com>
2024-01-30 14:46:22 +0545

    Linting Changed

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,8 +9,10 @@ from app import calc
 class CalcTest(SimpleTestCase):
     """Test the calc module"""
 
+
     def test_add_numbers(self):
         """test adding numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
+        
